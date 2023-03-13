b2eec576ba8a69ecd1f93a6c42ca11929581a000
Lucas Carro <rylcarro@gmail.com>
2023-03-12 23:24:34 -0300

    feat TP4

---
+++ agregar.js
@@ -0,0 +1,9 @@
+/*4) En un archivo llamado "agregar.js", importar el array que se encuentra en dataBase.js y crear una 
+función que me permita agregar un producto a la lista. Ingresando por parámetros, 
+el id, nombre, descripcion, categoria y precio. mostrarlo en un console.log*/
+
+const data = requiere("./dataBase")
+
+function addItem(id, nombre, descripcion, categoria, precio){
+  
+}
